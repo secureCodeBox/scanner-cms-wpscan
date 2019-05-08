500333b8aa27407f198c1b5dc63f95dca2981314
dpatanin <daniel.patanin@iteratec.com>
2019-05-08 13:49:13 +0200

    changed enumerate parameter hand over

--- src/wordpress_configuration.rb
+++ src/wordpress_configuration.rb
@@ -13,17 +13,21 @@ class WordpressConfiguration
   def self.from_target(job_id, target)
     config = WordpressConfiguration.new
 
+    enumerate = []
+    target.dig('attributes', 'WP_ENUMERATE').each { |_, flag| enumerate << flag unless flag.nil?  }
+
     config.job_id = job_id
     config.wordpress_scanner_target = target.dig('location')
     config.wordpress_configuration = ""
-    config.wordpress_configuration += '--stealthy ' if target.dig('attributes', 'WP_STEALTHY') == 'true'
-    config.wordpress_configuration += "--enumerate #{target.dig('attributes', 'WP_ENUMERATE')} " unless !target.dig('attributes', 'WP_ENUMERATE')
+    config.wordpress_configuration += '--stealthy ' if target.dig('attributes', 'WP_STEALTHY')
+    config.wordpress_configuration += "--enumerate #{enumerate.join(",")} " unless enumerate.empty?
     config.wordpress_configuration += "--max-scan-duration #{target.dig('attributes', 'WP_MAX_DURATION')} " unless !target.dig('attributes', 'WP_MAX_DURATION')
     config.wordpress_configuration += "--throttle #{target.dig('attributes', 'WP_THROTTLE')} " unless !target.dig('attributes', 'WP_THROTTLE')
     config.wordpress_configuration += "--request-timeout #{target.dig('attributes', 'WP_REQUEST_TIMEOUT')} " unless !target.dig('attributes', 'WP_REQUEST_TIMEOUT')
     config.wordpress_configuration += "--detection-mode #{target.dig('attributes', 'WP_DETECTION_MODE')} " unless !target.dig('attributes', 'WP_DETECTION_MODE')
     config.wordpress_configuration += "--ua #{target.dig('attributes', 'WP_USER_AGENT')} " unless !target.dig('attributes', 'WP_USER_AGENT')
     config.wordpress_configuration += "--headers #{target.dig('attributes', 'WP_HEADERS')} " unless !target.dig('attributes', 'WP_HEADERS')
+
 
     config
   end
--- src/wordpress_result_transformer.rb
+++ src/wordpress_result_transformer.rb
@@ -10,54 +10,55 @@ class WordpressResultTransformer
 
   def transform(r, timed_out: false)
     @findings = []
-        @findings << {
-            id: @uuid_provider.uuid,
-            name: 'CMS Wordpress',
-            description: 'CMS Wordpress Information',
-            category: 'CMS Wordpress',
-            osi_layer: 'NETWORK',
-            severity: 'INFORMATIONAL',
-            reference: {},
-            hint: '',
-            location: r.dig('target_url'),
-            attributes: {
-                requests_done: r.dig('requests_done').to_s,
-                db_update_finished: r.dig('db_update_finished').to_s,
-                version: r.dig('version', 'number').to_s,
-                start_time: Time.at(r.dig('start_time')).to_s,
-                end_time: Time.at(r.dig('stop_time')).to_s
-            }
-        }
-        unless r.dig('version').nil? or r.dig('version').empty?
-          vulnerabilitiesToFindings(r.dig('version', 'vulnerabilities'), 'HIGH') unless r.dig('version', 'vulnerabilities').nil? or r.dig('version', 'vulnerabilities').empty?
-        end
-        if r.dig('main_theme','version').nil? or r.dig('main_theme','version').empty?
-          vulnerabilitiesToFindings(r.dig('main_theme', 'vulnerabilities'), 'INFORMATIONAL') unless r.dig('main_theme', 'vulnerabilities').nil? or r.dig('main_theme', 'vulnerabilities').empty?
-        else
-          vulnerabilitiesToFindings(r.dig('main_theme', 'version', 'vulnerabilities'),'HIGH') unless r.dig('main_theme', 'version', 'vulnerabilities').nil? or r.dig('main_theme', 'version', 'vulnerabilities').empty?
-        end
-        unless r.dig('plugins').nil? or r.dig('plugins').empty?
-          r.dig('plugins').each do |p|
-            if p[1].dig('version').nil? or p[1].dig('version').empty?
-                vulnerabilitiesToFindings(p[1].dig('vulnerabilities'), 'INFORMATIONAL') unless p[1].dig('vulnerabilities').nil? or p[1].dig('vulnerabilities').empty?
-              else
-                vulnerabilitiesToFindings(p[1].dig('version', 'vulnerabilities'), 'HIGH') unless p[1].dig('version', 'vulnerabilities').nil? or p[1].dig('version', 'vulnerabilities').empty?
-              end
-          end
-        end
 
-      if timed_out
-        @findings = [{
-         id: @uuid_provider.uuid,
-         name: "Wordpress Scan timed out and could no be finished.",
-         description: "Wordpress Scan didnt send any new requests for 5 minutes. This probably means that wpscan encountered some internal errors it could not handle.",
-         osi_layer: 'NOT_APPLICABLE',
-         severity: "MEDIUM",
-         category: "ScanError",
-         hint: "This could be related to a misconfiguration.",
-         attributes: {}
-         }]
+    @findings << {
+        id: @uuid_provider.uuid,
+        name: 'CMS Wordpress',
+        description: 'CMS Wordpress Information',
+        category: 'CMS Wordpress',
+        osi_layer: 'NETWORK',
+        severity: 'INFORMATIONAL',
+        reference: {},
+        hint: '',
+        location: r.dig('target_url'),
+        attributes: {
+            requests_done: r.dig('requests_done').to_s,
+            db_update_finished: r.dig('db_update_finished').to_s,
+            version: r.dig('version', 'number').to_s,
+            start_time: Time.at(r.dig('start_time')).to_s,
+            end_time: Time.at(r.dig('stop_time')).to_s
+        }
+    }
+    unless r.dig('version').nil? or r.dig('version').empty?
+      vulnerabilitiesToFindings(r.dig('version', 'vulnerabilities'), 'HIGH') unless r.dig('version', 'vulnerabilities').nil? or r.dig('version', 'vulnerabilities').empty?
+    end
+    if r.dig('main_theme','version').nil? or r.dig('main_theme','version').empty?
+      vulnerabilitiesToFindings(r.dig('main_theme', 'vulnerabilities'), 'INFORMATIONAL') unless r.dig('main_theme', 'vulnerabilities').nil? or r.dig('main_theme', 'vulnerabilities').empty?
+    else
+      vulnerabilitiesToFindings(r.dig('main_theme', 'version', 'vulnerabilities'),'HIGH') unless r.dig('main_theme', 'version', 'vulnerabilities').nil? or r.dig('main_theme', 'version', 'vulnerabilities').empty?
+    end
+    unless r.dig('plugins').nil? or r.dig('plugins').empty?
+      r.dig('plugins').each do |p|
+        if p[1].dig('version').nil? or p[1].dig('version').empty?
+            vulnerabilitiesToFindings(p[1].dig('vulnerabilities'), 'INFORMATIONAL') unless p[1].dig('vulnerabilities').nil? or p[1].dig('vulnerabilities').empty?
+          else
+            vulnerabilitiesToFindings(p[1].dig('version', 'vulnerabilities'), 'HIGH') unless p[1].dig('version', 'vulnerabilities').nil? or p[1].dig('version', 'vulnerabilities').empty?
+          end
       end
+    end
+
+    if timed_out
+      @findings = [{
+       id: @uuid_provider.uuid,
+       name: "Wordpress Scan timed out and could no be finished.",
+       description: "Wordpress Scan didnt send any new requests for 5 minutes. This probably means that wpscan encountered some internal errors it could not handle.",
+       osi_layer: 'NOT_APPLICABLE',
+       severity: "MEDIUM",
+       category: "ScanError",
+       hint: "This could be related to a misconfiguration.",
+       attributes: {}
+       }]
+    end
     @findings
   end
 
