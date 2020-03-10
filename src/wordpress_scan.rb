da4e7f3fc6d066e197c7b69e63f5890fe33e7653
J12934 <13718901+J12934@users.noreply.github.com>
2020-03-10 14:03:08 +0100

    Use ternary to make codacy happy

--- src/wordpress_scan.rb
+++ src/wordpress_scan.rb
@@ -6,7 +6,7 @@ require 'pathname'
 require_relative './wordpress_result_transformer'
 
 $logger = Logger.new(STDOUT)
-$logger.level = if ENV.key? 'DEBUG' then Logger::DEBUG else Logger::INFO end
+$logger.level = ENV.key? 'DEBUG' ? Logger::DEBUG : Logger::INFO
 
 class WordpressScan
 	attr_reader :raw_results
